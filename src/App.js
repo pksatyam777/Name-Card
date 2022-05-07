39e0cf209b77c6242ae64b35dfd0fb022ec7148b
pksatyam777 <pksatyam777@gmail.com>
2022-05-07 17:14:11 +0530

    initial card save

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div classname="outershell">
+    <main className="container">
+      <div className="card-wrapper">
+        <div className="content">
+          <h3>Satyam S. </h3>
+          <p>Frontend Developer</p>
+          <a href="google.com">Read More</a>
+        </div>
+      </div>
+    </main>
+    </div>
+  );
+}
